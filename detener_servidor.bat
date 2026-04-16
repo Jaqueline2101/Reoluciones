b2f47b2dba7c163a8e6a6b64e4f9a874f0338d2b
Jaqueline2101 <jaqueline.21623@gmail.com>
2026-04-16 14:43:31 -0500

    logro

--- detener_servidor.bat
+++ detener_servidor.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Deteniendo el servidor de Analisis de Resoluciones en segundo plano...
-wmic process where "commandline like '%%python%%' and commandline like '%%app.py%%'" call terminate
+powershell -Command "Get-CimInstance Win32_Process | Where-Object { $_.CommandLine -match 'python.*app\.py' } | Invoke-CimMethod -MethodName Terminate"
 echo Servidor detenido.
 pause
